bff9b386c0d3072f48c2d284efc1765df621cfdc
Zocke07 <91361456+Zocke07@users.noreply.github.com>
2023-06-15 01:03:21 +0800

    Update BarByYear.py

--- BarByYear.py
+++ BarByYear.py
@@ -1,14 +1,10 @@
-import csv
 import chardet
-import matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Detect the encoding of the file
 with open('u2010im.csv', 'rb') as file:
     result = chardet.detect(file.read())
 
-# Read the file with the detected encoding
 data = pd.read_csv('u2010im.csv', encoding=result['encoding'])
 data1 = data
 year = []
@@ -24,9 +20,6 @@ ax.bar(year, total)
 ax.set_xticklabels(year, rotation='vertical')
 ax.ticklabel_format(style='plain', axis='y')
 plt.title("Import Trade Value")
-plt.xlabel("Imports from Country (Unit : US$ Thousand)")
-plt.ylabel("總計")
-plt.show()
-
-ax.pie(total, labels=year, autopct='%1.1f%%')
+plt.xlabel("Year")
+plt.ylabel("Imports from Country (Unit : US$ Thousand)")
 plt.show()
